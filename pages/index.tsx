ea205af0e73f4e6c1fa1b21112771ece90955ca1
Yogesh Dabral <yogesh@Yogeshs-MacBook-Pro-2.local>
2022-12-15 16:41:34 +0530

    Intial project structure for CWFiddle

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+function HomePage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      CWFiddle under construction.
+    </div>
+  );
+}
+
+export default HomePage;
